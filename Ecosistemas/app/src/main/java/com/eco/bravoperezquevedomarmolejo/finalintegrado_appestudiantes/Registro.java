86187964eb68dd4bfb7b39362958da6c36dbcc37
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-05-25 16:08:34 -0500

    Registro usuario funcionando firebase
    Se obtienen los datos del campo de texto, se suben a firebase y se puede registrar exitosamente el usuario

--- Ecosistemas/app/src/main/java/com/eco/bravoperezquevedomarmolejo/finalintegrado_appestudiantes/Registro.java
+++ Ecosistemas/app/src/main/java/com/eco/bravoperezquevedomarmolejo/finalintegrado_appestudiantes/Registro.java
@@ -2,18 +2,27 @@ package com.eco.bravoperezquevedomarmolejo.finalintegrado_appestudiantes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Registro extends AppCompatActivity {
 
+    private FirebaseDatabase db;
+    private DatabaseReference ref;
     private ImageButton registrar;
     private EditText codigo;
     private EditText correo;
     private EditText contra1;
     private EditText contra2;
-
-
+    private String codigos;
+    private String correos;
+    private String contras1;
+    private String contras2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,29 @@ public class Registro extends AppCompatActivity {
         correo=findViewById(R.id.edt_correo_registro);
         contra1=findViewById(R.id.edt_contra1_registro);
         contra2=findViewById(R.id.edt_contra2_registro);
+        db= FirebaseDatabase.getInstance();
+        ref= db.getReference();
 
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codigos= codigo.getText().toString().trim();
+                correos= correo.getText().toString().trim();
+                contras1= contra1.getText().toString().trim();
+                contras2= contra2.getText().toString().trim();
+                if(contras1.equals(contras2)){
+                    crearUsuario();
+                }else{
+                    Toast.makeText(Registro.this,"Error confirmacion contraseña",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
+    }
+
+    public void crearUsuario(){
+        Usuario user= new Usuario(codigos,correos,contras1,contras2);
+        ref.child("Usuarios").child(codigos).setValue(user);
     }
 
 }
