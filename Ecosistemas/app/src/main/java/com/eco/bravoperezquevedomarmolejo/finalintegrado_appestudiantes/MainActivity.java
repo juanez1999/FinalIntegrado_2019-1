a826f44c2a4b9a2efd9b0e02a09ff0fe3e744120
Juan Bravo <juanda.bravo98@gmail.com>
2019-05-25 15:38:06 -0500

    Revert "Merge branch 'master' of https://github.com/juanez1999/FinalIntegrado_2019-1"
    This reverts commit 8a989c0c03b09eecbfb7d9c23b2fe364a531e3ad, reversing
    changes made to d5b4e65ea1a302ed648aeb6377ce2f2b54e36605.

--- Ecosistemas/app/src/main/java/com/eco/bravoperezquevedomarmolejo/finalintegrado_appestudiantes/MainActivity.java
+++ Ecosistemas/app/src/main/java/com/eco/bravoperezquevedomarmolejo/finalintegrado_appestudiantes/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(MainActivity.this, Registro.class);
+                startActivity(i);
             }
         });
     }
