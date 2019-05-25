3ee4cc573d8900bc1a35cfa73fd67481eb81dd03
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-05-25 18:35:40 -0500

    Se agrega pantalla de perfil, aun no terminada
    Debe terminarse la pantalla de perfil, aun ninguna pantalla manda hacia el perfil

---
+++ Ecosistemas/app/src/main/java/com/eco/bravoperezquevedomarmolejo/finalintegrado_appestudiantes/Perfil.java
@@ -0,0 +1,13 @@
+package com.eco.bravoperezquevedomarmolejo.finalintegrado_appestudiantes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
