bb61eff4d05513b88288724d65e16d13a3ba3f64
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-05-30 00:34:40 -0500

    nada, basura esto

---
+++ Ecosistemas/app/src/main/java/com/eco/bravoperezquevedomarmolejo/finalintegrado_appestudiantes/Activities/Actividadespercepcion.java
@@ -0,0 +1,13 @@
+package com.eco.bravoperezquevedomarmolejo.finalintegrado_appestudiantes.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividadespercepcion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividadespercepcion2);
+    }
+}
