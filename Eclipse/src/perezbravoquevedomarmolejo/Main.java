4ff540688fabfb7c4b2f9dd95e1489480707c839
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-05-30 06:34:50 -0500

    Intento de la logica de la app de eclipse
    Intento base de como se pintaran los gifs y las imagenes con el mensaje recibido

--- Eclipse/src/perezbravoquevedomarmolejo/Main.java
+++ Eclipse/src/perezbravoquevedomarmolejo/Main.java
@@ -1,17 +1,21 @@
 package perezbravoquevedomarmolejo;
 
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet implements Observer{
 	
 	private static Comunicacion com;
+	private ArrayList<PImage> imagenes;
+	private int variablePintar;
 	
 	public static void main(String[] args) {
-		PApplet.main("main.Main");
+		PApplet.main("Main");
 		com= new Comunicacion();
 		Thread t = new Thread(com);
 		t.start();
@@ -23,17 +27,27 @@ public class Main extends PApplet implements Observer{
 	}
 	
 	public void setup() {
-		
+		imagenes = new ArrayList<PImage>();
 	}
 	
 	public void draw() {
-		
+		background(255);
+		for (int i = 0; i < imagenes.size(); i++) {
+			if(variablePintar==1) {
+				
+			}
+		}
+	
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		String notificacion = (String) arg1;
 		System.out.println(notificacion);
+		
+		if(notificacion.equals("EjemploContinuidad")) {
+			variablePintar=1;
+		}
 	}
 
 }
